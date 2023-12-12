f708df89454e32e85a44719693fca70686461375
Christian Montecchiani <christian.montecchiani99@gmail.com>
2023-12-12 22:49:29 +0100

    [Major] Implemented MCTS integrated with Connect4State

--- mcts.py
+++ mcts.py
@@ -1,160 +1,120 @@
 import numpy as np
+import math
+import random
 
-class Node():
+from connect4state import ConnectFourState
 
-    def __init__(self, state, parent = None) -> None:
+class MCTSNode:
+    def __init__(self, state, parent=None):
         self.state = state
         self.parent = parent
         self.children = []
-        self.visits = 0  # prob of being selected
-        self.value = 0.0
-        self.terminal = False
-    
+        self.visits = 0
+        self.value = 0
 
-    def is_fully_expanded(self):
-        """ Check if the node is fully expanded """
-        return len(self.children) == len(self.state.get_legal_actions())
-    
-
-    def best_child(self, exploration_weight = 1.0):
-        """ Return the best child according to Upper Confidence Bound (UCB) """
-        if not self.children:
-            return None
-
-        def ucb_score(child):
-            exploitation = child.value/child.visits if child.visits > 0 else 0
-            exploration = exploration_weight * np.sqrt(2*np.log(self.visits)/child.visits) if child.visits > 0 else 0
-            return exploitation + exploration
-
-        return max(self.children, key=ucb_score)
-
-
-    def select_unexplored_child(self):
-        """ Return an unexplored child """
-        legal_actions = self.state.get_legal_actions()
-        explored_actions = [child.state.last_action for child in self.children]
-        unexplored_actions = [act for act in legal_actions if act not in explored_actions]
-
-        if unexplored_actions:
-            action = np.random.choice(unexplored_actions)
-            return Node(self.state.perform_action(action), parent=self)
+    def print_tree(self, indent="", last='updown'):
+        print(indent, end="")
+        if last == 'updown':
+            print("└──", end="")
+            indent += "    "
         else:
-            return None
+            print("├──", end="")
+            indent += "|   "
 
-    def expand(self):
-        """ Expand the node by adding a new child """
-        unexplored_child = self.select_unexplored_child()
-        if unexplored_child:
-            self.children.append(unexplored_child)
-            return unexplored_child
-        else:
-            return None
-    
-    def simulate(self):
-        """ Simulate the game from the current state """
-        current_state = self.state
-        while not current_state.is_terminal():
-            random_action = random.choice(current_state.get_legal_actions())
-            current_state = current_state.perform_action(random_action)
-        return current_state.get_reward()            
+        print(f"visits: {self.visits}, value: {self.value}, move: {self.state.last_move}")
 
+        child_count = len(self.children)
+        for i, child in enumerate(self.children):
+            last = 'updown' if i == child_count - 1 else 'leftright'
+            child.print_tree(indent, last)
 
-    def backpropagate(self, reward):
-        """ Update the node statistics """
-        node = self 
-        while node is not None:
-            node.visits += 1
-            node.value += reward
-            node = node.parent  
-        
-
-    # def add_game(self, is_win):
-    #     if is_win:
-    #         self.value += 1
-    #     self.plays+=1
-
-
-class MonteCarloTreeSearch():
-
-    def __init__(self, root_state, exploration_weight=1.0, iterations=1000):
-        self.root = Node(root_state)
-        self.exploration_weight = exploration_weight
-        self.iterations = iterations
-        
-    
-    def search(self):
-        for _ in range(self.iterations):
-            node = self.select_node_to_expand()
-            if node is not None:
-                new_node = node.expand()
+def mcts_search(root, num_iterations=1):
+    for _ in range(num_iterations):
+        node = root
+        count = 0 
+        while not node.state.is_terminal():
+            count+=1
+            if not node.children:
+                # Expand the node if it's not fully expanded
+                node.print_tree()
+                new_node = expand(node)
                 if new_node is not None:
-                    reward = new_node.simulate()
-                    new_node.backpropagate(reward)
-        best_child = self.root.best_child(exploration_weight=0.0)
-        return best_child.state.last_action
+                    node = new_node
+                else:
+                    break
 
+            else:
+                # Select the child node with the highest UCB score
+                node = select(node)
 
-    def select_node_to_expand(self):
-        """ Select a node to expand """
-        node = self.root
-        while node.is_fully_expanded() and not node.state.is_terminal():
-            node = node.best_child(self.exploration_weight)
-        return node
-        
+        # Simulate from the selected node to the end of the game
+        result = simulate(node.state)
 
-class GameState:
+        # Backpropagate the result through the tree
+        backpropagate(node, result)
+
+    # Choose the best move based on the most visited child
+    best_child = max(root.children, key=lambda child: child.visits)
+    return best_child.state.last_move
+
+def expand(node):
+    """Expand by adding a child corresponding to a valid move"""
+    valid_moves = node.state.get_valid_moves()
     
-    def __init__(self, obs, conf, trainer):
-        self.state = obs["board"]
-        self.trainer = trainer
-        self.conf = conf
-        self.is_terminal_state = False 
-        self.last_action = None
+    if valid_moves:
+        move = random.choice(valid_moves)
+        new_state = node.state.copy_and_apply_move(move)
+        child = MCTSNode(new_state, parent=node)
+        node.children.append(child)
+        return child
+    else:
+        # Handle the case where there are no valid moves left
+        return None  # Returning the current node without expansion
 
-    
-    def get_legal_actions(self):
-        return [i for i, col in enumerate(self.state[:self.conf['columns']]) if col == 0]
+def select(node):
+    """ Use the Upper Confidence Bound  formula to select a child"""
+    exploration_weight = 1.0
+    ucb_scores = [child.value / child.visits + exploration_weight * math.sqrt(math.log(node.visits) / child.visits)
+                if child.visits > 0 else float('inf') for child in node.children]
+    selected_child = node.children[np.argmax(ucb_scores)]
+    return selected_child
 
-    def perform_action(self, action):
-        obs, reward, done, info = self.trainer.step(action)
-        new_state = GameState(obs, self.conf, self.trainer)
-        new_state.last_action = action
-        new_state.is_terminal_state = done
-        
-        if done:
-            obs = trainer.reset()
-        return new_state
+def simulate(state):
+    # Simulate the game from the current state until a terminal state is reached
+    while not state.is_terminal():
+        valid_moves = state.get_valid_moves()
+        move = random.choice(valid_moves)
+        state = state.copy_and_apply_move(move)
+    return state.get_result()
 
-    def is_terminal(self):
-        return self.is_terminal_state
-
-    def get_reward(self):
-        return 1.0 if self.is_terminal_state else 0.0
-
-    def copy(self):
-        pass
-
-from kaggle_environments import evaluate, make, utils
+def backpropagate(node, result):
+    # Backpropagate the result through the tree
+    while node is not None:
+        node.visits += 1
+        node.value += result
+        node = node.parent
 
 
-env = make("connectx", debug=True)
-trainer = env.train(["random", None])
-obs = trainer.reset()
-done = False
-conf = env.configuration
-env.render()
-initial_state = GameState(obs, conf, trainer=trainer)
-mcts = MonteCarloTreeSearch(initial_state, iterations=1000)
-action = mcts.search()
-obs, reward, done, info = trainer.step(action)
-env.render()
-print(action)
 
-# while not done:
-#     a = agent.act(obs)
-#     obs, reward, done, info = trainer.step(a)
-#     print(obs, reward, done, info)
-#     env.render()
-#     if done:
-#         obs = trainer.reset()
-# env.render()
+if __name__ == "__main__":
+    game_state = ConnectFourState()
+    root_node = MCTSNode(game_state)
+
+    while not game_state.is_terminal():
+        # Use MCTS to find the best move
+        best_move = mcts_search(root_node, num_iterations=100)
+
+        # Apply the best move to the current state
+        game_state.make_move(best_move)
+
+        # Update the root node for the next iteration
+        root_node = next((child for child in root_node.children if child.state.last_move == best_move), None)
+
+        # Print the current state of the game
+        print(game_state.board)
+
+    winner = game_state.check_winner()
+    if winner:
+        print(f"Player {winner} wins!")
+    else:
+        print("It's a tie!")
